b7ec91c3287290643f854e5e99dac0171b4689e1
Vincent Kioko <vincentkioko51@gmail.com>
2023-09-19 17:11:46 +0300

    Apollo Server - Type Definitions

--- src/index.js
+++ src/index.js
@@ -9,18 +9,41 @@ app.use(cors())
 
 const schema = gql`
     type Query {
+        users: [User!]
         me: User
+        user(id: ID!): User
     }
 
     type User {
+        id: ID!
         username: String!
     }
 `;
+
+let users = {
+    1: {
+        id: '1',
+        username: 'Sntaks'
+    },
+    2: {
+        id: '2',
+        username: 'Vincent'
+    }
+}
+
+const me = users[1]
+
 const resolvers = {
     Query: {
-        me: () => ({
-            username: process.env.USERNAME
-        })
+        users: () => {
+            return Object.values(users)
+        },
+        user: (parent, { id }) => {
+            return users[id];
+        },
+        me: () => {
+            return me
+        },
     }
 }
 
